e4c93264ea5dc0d7b933f5abdfd210827e5c58cc
ViniciuMota <vinimota44@gmail.com>
2023-12-22 17:43:30 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Viniciumota.png" />
-    <Titulo fontSize={20}>Vinicius Mota</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Vinicius Mota</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Viniciumota
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro de Software
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
---
+++ src/containers/Sidebar/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+import Paragrafo from '../../components/Paragrafo'
+
+export const Descricao = styled(Paragrafo)`
+  margin-top: 24px;
+  margin-bottom: 40px;
+`
+export const BotaoTema = styled.button`
+  border-radios: 12px;
+  padding: 8px;
+  color: #eee;
+  font-size: 10px;
+  font-weight: bold;
+  background-color: #282a35;
+  cursor: pointer;
+`
+export const SidebarContainer = styled.div`
+  position: sticky;
+  top: 80px;
+  left: 0;
+`
