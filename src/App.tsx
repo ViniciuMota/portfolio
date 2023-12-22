b8374f503d0e223f885a8101ee84bb5919bc61d4
ViniciuMota <vinimota44@gmail.com>
2023-12-22 18:35:42 -0300

    adição de estilo dark e responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,28 @@ import Sidebar from './containers/Sidebar'
 import Projetos from './containers/Projetos'
 import Sobre from './containers/Sobre'
 import EstiloGlobal, { Container } from './styles'
+import { ThemeProvider } from 'styled-components'
+import temaLight from './themes/light'
+import temaDark from './themes/dark'
+import { useState } from 'react'
 
 function App() {
+  const [estaUsandoTemaDark, setEstaUsandoTemaDark] = useState(false)
+
+  function trocaTema() {
+    setEstaUsandoTemaDark(!estaUsandoTemaDark)
+  }
   return (
-    <>
+    <ThemeProvider theme={estaUsandoTemaDark ? temaDark : temaLight}>
       <EstiloGlobal />
       <Container>
-        <Sidebar />
+        <Sidebar trocaTema={trocaTema} />
         <main>
           <Sobre />
           <Projetos />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
